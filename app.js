9027d0f0f5e32fd4df1ff2f5d1c2055b3eef3e96
Santosh <nandiyawarsantosh.0719@gmail.com>
2024-02-06 00:39:00 +0530

    Create:- OTP generated

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 const express = require("express");
+const nodemailer = require("nodemailer");
+const mailGen = require("mailgen");
 // const mongoose = require("mongoose");
 const app = express();
 const User = require("./models/userModel");
 const userRoutes = require("./api/routes/user");
 app.use(express.json());
 // app.use("/users", userRoutes);
+const { EMAIL, PASSWORD } = require("./env.js");
+const Todos = require("./models/todoModel");
 
 // cors = Cross-origin resource sharing
 const cors = require("cors");
-const Todos = require("./models/todoModel");
 const corsOption = {
   origin: ["http://localhost:3000"],
+  // origin: ["https://my-json-server.typicode.com/santoshlearner07/users_api"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
 };
-
 app.use(cors(corsOption));
 
 app.get("/users", async (req, res) => {
@@ -26,9 +29,58 @@ app.get("/users", async (req, res) => {
   }
 });
 
+// const getMail = (req,res)=>{
+app.post("/postotp", async (req, res) => {
+  const userEmail = req.body;
+  console.log(userEmail);
+  let config = {
+    service: "gmail",
+    auth: {
+      user: EMAIL,
+      pass: PASSWORD,
+    },
+  };
+
+  let transporter = nodemailer.createTransport(config);
+
+  let mailGenerator = new mailGen({
+    theme: "default",
+    product: {
+      name: "Multi User",
+      link: "https://mailgen.js/",
+    },
+  });
+
+  let response = {
+    body: {
+      name: "Santosh",
+      intro: "Your One Time Password",
+      outro: "112233",
+    },
+  };
+
+  let mail = mailGenerator.generate(response);
+
+  let message = {
+    from: EMAIL,
+    to: "abc@gmail.com",
+    subject: "One Time Password",
+    html: mail,
+  };
+  transporter
+    .sendMail(message)
+    .then(() => {
+      return res.status(201).json({
+        msg: "You Should receive an email",
+      });
+    })
+    .catch((err) => {
+      return res.status(500).json({ err });
+    });
+  // res.status(201).json("Get Mail Successfully");
+});
+
 app.post("/users", async (req, res) => {
-  // console.log(req.body)
-  // res.send(req.body)
   try {
     const user = await User.create(req.body);
     res.status(200).json(user);
@@ -96,17 +148,15 @@ app.put("/todos/:userId/:todoId", async (req, res) => {
   try {
     const userId = req.params.userId;
     const todoId = req.params.todoId;
-    const todoItem = await Todos.replaceOne({ userId, todoId },req.body);
+    const todoItem = await Todos.replaceOne({ userId, todoId }, req.body);
     if (!todoItem) {
       return res.status(404).json({ message: "Cannot find todo item." });
     }
     res.status(200).json(todoItem);
-    console.log(todoItem)
+    console.log(todoItem);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
-
-
 
 module.exports = app;
---
+++ env.js
@@ -0,0 +1,4 @@
+module.exports = {
+    EMAIL:'santoshwalker719@gmail.com',
+    PASSWORD:'vcvhjykdgixkhxsv'
+}
